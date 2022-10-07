5d47de8fd7cc6fb0fa24d1b32149dd0a80759ab4
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-10-07 09:47:26 +0300

    Added useEffect hook to fetch data from db.json

--- src/Components/MovieContainer.js
+++ src/Components/MovieContainer.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import AddMovie from "./AddMovie";
 import Movie from "./Movie";
 import Search from "./Search";
@@ -9,7 +9,13 @@ function MovieContainer() {
 
     const [movieData, setMovieData] = useState([]);
 
-
+    useEffect(() => {
+        fetch("http://localhost:4000/movies")
+          .then((res) => res.json())
+          .then((movieData) => {
+            setMovieData(movieData)
+          })
+      }, []);
 
 
 
